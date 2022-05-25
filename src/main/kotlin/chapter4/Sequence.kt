69c25d5690902bc891c891cef21896e9ed07b163
Kamila Santos <kahsantos89@gmail.com>
2022-05-25 06:26:04 -0300

    ex: sequence

---
+++ src/main/kotlin/chapter4/Sequence.kt
@@ -0,0 +1,16 @@
+package chapter4
+
+fun main() {
+    val result = (100 until 200).asSequence()
+        .map { println("doubling $it"); it * 2 }
+        .filter { println("filtering $it"); it % 3 == 0 }
+        .first()
+    println("results = $result")
+    println()
+
+    println()
+    val myThrees = sequenceOf(1,3,5,7,9).filter { it %3 ==0 }
+    println("myThrees = $myThrees")
+    println(myThrees.toList())
+
+}
